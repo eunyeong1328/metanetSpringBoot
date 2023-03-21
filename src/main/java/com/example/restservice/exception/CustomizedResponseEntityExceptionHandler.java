0e603d5bc812289d7f15eec5bf1d07d0276a782c
eunyoung1328 <eunyeong1328@daum.net>
2023-03-21 14:53:11 +0900

    feat:모든 에러 핸들링 제공 클래스

---
+++ src/main/java/com/example/restservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.restservice.exception;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+//에러를 핸들링 하기 위해 스프링부트에서 제공하는 클래스
+//모든 컨트롤러가 실행이 될 때 @ControllerAdvice가 예외를 전담해서 처리
+@RestController
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    //모든 예외처리를 처리해 주는 메서드
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+        //예외 메세지
+        ExceptionResponse exceptionResponse 
+                = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+                                                        //예외 발생 출력, 서버 에러
+    }
+}
--- src/main/java/com/example/restservice/user/UserNotFoundException.java
+++ src/main/java/com/example/restservice/user/UserNotFoundException.java
@@ -1,11 +1,17 @@
 package com.example.restservice.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 //2xx => ok
 //4xx => client error (ex json값 잘못 날리면
 //5xx => server error
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException  extends RuntimeException{
     //예외 발생 클래스
     public UserNotFoundException(String message){
         super(message);
     }
+
 }
