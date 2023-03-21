c8d501908d4a851c866f6966ed4dea3f5f730fe5
eunyoung1328 <eunyeong1328@daum.net>
2023-03-21 16:46:38 +0900

    Feat: User 객체 필터링

--- src/main/java/com/example/restservice/user/AdminController.java
+++ src/main/java/com/example/restservice/user/AdminController.java
@@ -1,4 +1,72 @@
 package com.example.restservice.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.databind.util.SimpleBeanPropertyDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")//경로가 adminController일 때는 이렇게 구분
 public class AdminController {
+    @Autowired
+    private UserService service;
+    public AdminController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUser(@PathVariable int id){
+        User user = service.findone(id);
+
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+            //인위적으로 예외 발생 throw
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "ssn");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+
+    //이 uri 조회 시 방금 넣은 데이터 값 확인 가능
+    @PostMapping("/users")
+    public ResponseEntity createUser(@Valid @RequestBody User user){
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user == null){
+            throw new UserNotFoundException(String.format("%s ID not found",id));
+        }
+    }
+
 }
--- src/main/java/com/example/restservice/user/User.java
+++ src/main/java/com/example/restservice/user/User.java
@@ -1,5 +1,6 @@
 package com.example.restservice.user;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
@@ -13,13 +14,14 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIgnoreProperties(value = {"password","ssn"})
+//@JsonIgnoreProperties(value = {"password","ssn"})
+@JsonFilter("UserInfo") //UserInfo라고 필터
 public class User {
     private Integer id; //기본데이터형을 오브젝트로 변환
     @Size(min=2, message = "Name은 2글자 이상 입력하세요")
     private String name;
     private Date joinDate;
-    @JsonIgnore //예민한 값 노출 안되게 한다.
+    //@JsonIgnore //예민한 값 노출 안되게 한다.
     private  String password;
     private  String ssn; //주민등록번호
 }
