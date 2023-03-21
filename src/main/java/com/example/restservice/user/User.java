b60981ace5cc22a8885fda91a44b0d9d613ebc67
eunyoung1328 <eunyeong1328@daum.net>
2023-03-21 15:40:17 +0900

    feat:add validation

--- src/main/java/com/example/restservice/user/User.java
+++ src/main/java/com/example/restservice/user/User.java
@@ -3,7 +3,9 @@ package com.example.restservice.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Singular;
 
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -11,6 +13,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class User {
     private Integer id; //기본데이터형을 오브젝트로 변환
+    @Size(min=2, message = "Name은 2글자 이상 입력하세요")
     private String name;
     private Date joinDate;
 }
