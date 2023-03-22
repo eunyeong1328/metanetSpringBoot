4815885dfa562d4498b1c311911e80728b920bc8
eunyoung1328 <eunyeong1328@daum.net>
2023-03-22 11:14:10 +0900

    Feat:db의데이터 postman에서 확인

--- src/main/java/com/example/restservice/user/UserRepository.java
+++ src/main/java/com/example/restservice/user/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.restservice.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Repository
+@RequestMapping("/jpa")
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
