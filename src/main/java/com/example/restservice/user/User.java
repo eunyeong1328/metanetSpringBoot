d09aa04a1754e7202ca3359cff88c68bf272f9b8
eunyoung1328 <eunyeong1328@daum.net>
2023-03-22 10:38:06 +0900

    Feat: jpa db 설정

--- src/main/java/com/example/restservice/user/User.java
+++ src/main/java/com/example/restservice/user/User.java
@@ -8,6 +8,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Singular;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
@@ -16,7 +19,10 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonIgnoreProperties(value = {"password","ssn"})
 //@JsonFilter("UserInfo") //UserInfo라고 필터
+@Entity //entity 사용시 반드시 주시 설정
 public class User {
+    @Id
+    @GeneratedValue //1씩 증가/db에 따라 다르다(sql, 오라클)
     private Integer id; //기본데이터형을 오브젝트로 변환
     @Size(min=2, message = "Name은 2글자 이상 입력하세요")
     private String name;
